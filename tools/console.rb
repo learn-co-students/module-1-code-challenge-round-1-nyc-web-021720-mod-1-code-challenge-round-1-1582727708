0122628158f1730dda5dc8bd32ac671ae5fedfff
jguzz <joseph.a.guzzardo@gmail.com>
2020-02-26 09:55:57 -0500

    customer

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,10 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Joseph", "Guzzardo")
+c2 = Customer.new("Grace", "Guzzardo")
+c3 = Customer.new("Kara", "Dodson")
+c4 = Customer.new("Minaal", "Khan")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
