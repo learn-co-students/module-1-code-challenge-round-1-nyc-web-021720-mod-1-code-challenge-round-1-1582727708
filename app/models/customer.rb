8f861bc21f8a0ed124b0f63f4f621b52606d6021
devmullets <devmullets@gmail.com>
2020-02-26 10:01:28 -0500

    initial customer.rb scope finished

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,36 @@
 class Customer
+  #names are 'George Washington' format
+
   attr_accessor :given_name, :family_name
+
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+
+    Customer.all << self
   end
+
+  def self.all
+    @@all
+  end 
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def given_name
+    @given_name
+  end 
+
+  def family_name
+    @family_name
+  end 
+
+  def full_name
+    "#{@given_name} #{@family_name}"
+  end 
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,10 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+cust1 = Customer.new("Ryan", "Vaz")
+cust2 = Customer.new("Jason", "Sudeakis")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
