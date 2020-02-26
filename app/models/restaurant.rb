439ed1345d1494d43384dfbc0c1e63436afe7362
lucassherwin <lksherwin@gmail.com>
2020-02-26 10:43:14 -0500

    save4

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -22,7 +22,25 @@ class Restaurant
   end
 
   def average_star_rating
-    
+    #return average star rating for resturant 
+    #go over all reviews -> get rating -> add -> divide by num ratings
+    total = 0
+    reviews = Review.all.map do |review|
+      review
+    end
+    reviews.map do |review|
+      total += review.rating
+    end
+    total / reviews.size
+
+  #we want reviews to be an array of the ratings for a resturant
+  #   reviews = Review.all.map do |review_num|
+  #     review_num.rating
+  #   end
+  #   avg = reivews.inject{|sum, el| sum + el}.to_f /reviews.size
+  #   #avg
+
+  #  end
   end
 end
  
