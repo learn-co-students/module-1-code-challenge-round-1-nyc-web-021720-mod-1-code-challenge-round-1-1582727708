3656e75ad1f637ec6d219f052464b5b6f657205a
github username <github email address>
2020-02-26 10:51:23 -0500

    “save”

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,14 @@
 class Review
 
-attr_accessor :customer, :rating 
+attr_accessor :given_name, :family_name, :rating 
 attr_reader :restaurant 
   
 @@all = []
 
-    def initialize(customer, restaurant, rating)
-        @customer = customer
+    def initialize(given_name, family_name, restaurant, rating)
+        @given_name = given_name
+        @family_name = family_name
+        # @customer = customer
         @restaurant = restaurant
         @rating = rating
         Review.all << self 
