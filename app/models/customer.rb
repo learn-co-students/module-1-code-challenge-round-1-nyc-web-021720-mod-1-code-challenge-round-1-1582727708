68a4170e4146593d610cc983357ba1a809863225
danielxurbina <danielur1999@gmail.com>
2020-02-26 10:59:44 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
-  attr_accessor :given_name, :family_name
+  attr_accessor :first_name, :last_name
+  @@all = []
 
-  def initialize(given_name, family_name)
-    @given_name = given_name
-    @family_name  = family_name
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    Customer.all << self
   end
 
   def full_name
-    "#{given_name} #{family_name}"
+    "#{@first_name} #{@last_name}"
   end
+
+  def self.all
+    @@all 
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def review
+    Review.all.select do |restaurant|
+      restaurant.customer == self
+    end
+  end
+      
+  def restaurants
+    review.map do |reviews|
+      reviews.restaurant
+    end.uniq
+  end
+
+  def find_by_name(full_name)
+    Review.all.find do |customer_name|
+      customer_name.full_name == self
+    end
+  end
+
+  def find_all_by_given_name(first_name)
+    Review.all.map do |customer|
+      customer.full_name == self
+    end
+  end
+
+  def num_reviews
+    review.count
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    Restaurant.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def restaurant_reviews 
+    Review.all.select do |reviews|
+      reviews.restaurant == self
+    end
+  end
+
+  def reviews
+    restaurant_reviews.map do |restaurant|
+      restaurant.customer
+    end
+  end
+
+  def customers
+    Review.all.select do |customer|
+      customer.restaurant
+    end.uniq
+  end
+
+  def average_star_rating
+    Review.all.select do |rating|
+      rating.restaurant += (rating + rating / rating)
+    end
   end
 
 end
