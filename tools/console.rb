5887381d258e451c879f91c7c25c9235bf743292
github username <github email address>
2020-02-26 10:48:15 -0500

    “save”

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../config/environment.rb'
 
 def reload
@@ -8,7 +9,13 @@ vanessa_martinez = Customer.new("Vanessa", "Martinez")
 jeffrey_martinez = Customer.new("Jeffrey", "Martinez")
 patricia_rodriguez = Customer.new("Patricia", "Rogriguez")
 
+almond = Restaurant.new("Almond")
+jims = Restaurant.new("Jims")
+dons = Restaurant.new("Dons")
 
+review1 = Review.new("Vanessa", "Martinez", "Almond", 5)
+review2 = Review.new("Jeffrey", "Martinez", "Jims", 3)
+review3 = Review.new("Patricia", "Rodriguez", "Dons", 5)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
