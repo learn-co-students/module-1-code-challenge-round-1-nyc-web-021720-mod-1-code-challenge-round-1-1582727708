e63daa5a3a72fa707539eac29ab5da95c2d2a3bf
github username <github email address>
2020-02-26 10:38:30 -0500

    “save”

--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,12 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+vanessa_martinez = Customer.new("Vanessa", "Martinez")
+jeffrey_martinez = Customer.new("Jeffrey", "Martinez")
+patricia_rodriguez = Customer.new("Patricia", "Rogriguez")
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
